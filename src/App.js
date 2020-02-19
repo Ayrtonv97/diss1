0dc0960cd334ec2c565706429a6181aefd7bb91e
Ayrton <s4920259@bournemouth.ac.uk>
2020-02-19 18:30:37 +0000

    signIn button to be moved correctly

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
 import React from 'react';
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import './App.css';
-
-
-const gridExamplesPage = () => {
-return (
-    <MDBContainer>
-        <MDBRow>
-            <MDBCol md="3">.col-md-3</MDBCol>
-            <MDBCol md="4">.col-md-4</MDBCol>
-        </MDBRow>
-    </MDBContainer>
-);
-}
-export default gridExamplesPage()
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact";
+import './App.css'; //Import here your file styles
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+const GridExamplesPage = () => {
+    return (
+        <MDBContainer>
+            <MDBRow>
+                <MDBCol md="11">Food-Equator Calculator</MDBCol>
+            </MDBRow>
+        </MDBContainer>
+    );
+};
+
+
+const ButtonPage = () => {
+    return (
+        <Fragment>
+            <MDBBtn color="primary" className="float-right">signIn/login</MDBBtn>
+        </Fragment>
+    );
+};
+
+ReactDOM.render(<div> < GridExamplesPage/>, < ButtonPage/> </div>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
