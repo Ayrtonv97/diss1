c97fd0f4d46d1a8cb8e82dd389149e324ad6d98a
Ayrton <s4920259@bournemouth.ac.uk>
2020-02-23 16:36:09 +0000

    Option dropdown and research

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
 import React from 'react';
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import './App.css';
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavItem,
+    MDBNavLink,
+    MDBNavbarToggler,
+    MDBCollapse} from "mdbreact";
 import './App.css'; //Import here your file styles
-
-
+import { BrowserRouter as Router } from 'react-router-dom';
 const GridExamplesPage = () => {
     return (
         <MDBContainer>
@@ -15,19 +19,30 @@ const GridExamplesPage = () => {
         </MDBContainer>
     );
 };
-
-
 const ButtonPage = () => {
     return (
         <Fragment>
-            <MDBBtn color="primary" className="float-right">signIn/login</MDBBtn>
+            <MDBBtn color="primary" >Sign in/login</MDBBtn>
         </Fragment>
     );
 };
 
-ReactDOM.render(<div> < GridExamplesPage/>, < ButtonPage/> </div>, document.getElementById('root'));
+const Activity= () => {
+    return (
+        <select className="4">
+            <option>Choose activity</option>
+            <option value="1"> Running</option>
+            <option value="2"> Walking</option>
+            <option value="3"> Cycling</option>
+        </select>
+    );
+};
+
+ReactDOM.render(<div> < GridExamplesPage/> < ButtonPage/> <Activity/> </div>, document.getElementById('root'));
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
 
+serviceWorker.unregister();
