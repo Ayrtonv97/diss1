df01d4e8f03071bc280a206ed844d39c89870113
Ayrton <s4920259@bournemouth.ac.uk>
2020-02-24 12:48:23 +0000

    navbar stuff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,76 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavItem,
+    MDBNavLink,
+    MDBNavbarToggler,
+    MDBCollapse } from "mdbreact";
 import './App.css'; //Import here your file styles
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class HamburgerMenuPage extends Component {
+    state = {
+        collapseID: '',
+        memes: 'swagging'
+
+    };
+
+    toggleCollapse = collapseID => () => {
+        this.setState(prevState => ({
+            collapseID: prevState.collapseID !== collapseID ? collapseID : ''
+        }));
+    };
+
+    render() {
+        return (
+            <Router>
+                <MDBContainer>
+                    <MDBNavbar
+                        color='light-blue lighten-4'
+                        style={{ marginTop: '20px' }}
+                        light
+                    >
+                        <MDBContainer>
+                            <MDBNavbarBrand>{this.state.memes}</MDBNavbarBrand>
+                            <MDBNavbarToggler
+                                onClick={this.toggleCollapse('navbarCollapse1')}
+                            />
+                            <MDBCollapse
+                                id='navbarCollapse1'
+                                isOpen={this.state.collapseID}
+                                navbar
+                            >
+                                <MDBNavbarNav left>
+                                    <MDBNavItem active>
+                                        <MDBNavLink to='#!'>{this.state.memes}</MDBNavLink>
+                                    </MDBNavItem>
+                                    <MDBNavItem>
+                                        <MDBNavLink to='#!'>{this.state.memes}</MDBNavLink>
+                                    </MDBNavItem>
+                                    <MDBNavItem>
+                                        <MDBNavLink to='#!'>{this.state.memes}</MDBNavLink>
+                                    </MDBNavItem>
+                                </MDBNavbarNav>
+                            </MDBCollapse>
+                        </MDBContainer>
+                    </MDBNavbar>
+                </MDBContainer>
+            </Router>
+        );
+    }
+}
+
+class HamburgerMenuPage2 extends HamburgerMenuPage {
+
+    render() {
+        return super.render();
+    }
+}
 
 const GridExamplesPage = () => {
     return (
@@ -36,7 +102,7 @@ const Activity= () => {
     );
 };
 
-ReactDOM.render(<div> < GridExamplesPage/> < ButtonPage/> <Activity/> </div>, document.getElementById('root'));
+ReactDOM.render(<div> < HamburgerMenuPage/> < HamburgerMenuPage2/>  < GridExamplesPage/> < ButtonPage/> <Activity/> </div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
