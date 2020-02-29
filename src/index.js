3df90d5aa55731d0771cf2c1d4353082c4638c98
ayrtonvr <ayrtonvanraat@hotmail.co.uk>
2020-02-29 14:49:00 +0000

    Adding tab content

--- src/index.js
+++ src/index.js
@@ -11,68 +11,55 @@ import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBNavbar,
     MDBNavbarToggler,
     MDBCollapse } from "mdbreact";
 import './App.css'; //Import here your file styles
-
 import { BrowserRouter as Router } from 'react-router-dom';
 
 class HamburgerMenuPage extends Component {
-    state = {
-        collapseID: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapse: false
+        };
+        this.onClick = this.onClick.bind(this);
+    }
 
-    };
-
-    toggleCollapse = collapseID => () => {
-        this.setState(prevState => ({
-            collapseID: prevState.collapseID !== collapseID ? collapseID : ''
-        }));
-    };
+    onClick() {
+        this.setState({
+            collapse: !this.state.collapse,
+        });
+    }
 
     render() {
-        return (
+        const container = {height: 500}
+        return(
+            <div>
             <Router>
-                <MDBContainer>
-                    <MDBNavbar
-                        style={{ marginTop: '5px' }}
-                        light
-                    >
-                        <MDBContainer>
-                            <MDBNavbarBrand>Food Equator</MDBNavbarBrand>
-                            <MDBNavbarToggler
-                                onClick={this.toggleCollapse('navbarCollapse1')}
-                            />
-                            <MDBCollapse
-                                id='navbarCollapse1'
-                                isOpen={this.state.collapseID}
-                                navbar
-                            >
-                                <MDBNavbarNav left>
-                                    <MDBNavItem active>
-                                        <MDBNavLink to='#!'>Home</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem>
-                                        <MDBNavLink to='#!'>Activity</MDBNavLink>
-                                    </MDBNavItem>
-                                    <MDBNavItem>
-                                        <MDBNavLink to='#!'>Sign in/ Login</MDBNavLink>
-                                    </MDBNavItem>
-                                </MDBNavbarNav>
-                            </MDBCollapse>
-                        </MDBContainer>
+                <header>
+                    <MDBNavbar color="black" dark expand="md" >
+                        <MDBNavbarBrand href="/">
+                            <strong>Food-Equator Calculator</strong>
+                        </MDBNavbarBrand>
+                        <MDBNavbarToggler onClick={ this.onClick } />
+                        <MDBCollapse isOpen = { this.state.collapse } navbar>
+                            <MDBNavbarNav left>
+                                <MDBNavItem active>
+                                    <MDBNavLink to="#">Home</MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBNavLink to="#">My Log</MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBNavLink to="#">Sign in/Login</MDBNavLink>
+                                </MDBNavItem>
+                            </MDBNavbarNav>
+                        </MDBCollapse>
                     </MDBNavbar>
-                </MDBContainer>
+                </header>
             </Router>
+        </div>
         );
     }
 }
 
-const GridExamplesPage = () => {
-    return (
-        <MDBContainer>
-            <MDBRow>
-                <MDBCol md="11">Food-Equator Calculator</MDBCol>
-            </MDBRow>
-        </MDBContainer>
-    );
-};
 
 
 const ButtonPage = () => {
@@ -100,10 +87,39 @@ const Food= () => {
             <option value="1">Apple</option>
             <option value="2"></option>
         </select>
-    )
+    );
+}
+    function Tabs(){
+        return(
+            <div>
+            <h1>Tabs Demo</h1>
+            <Tabs>
+            <div label="Gator">
+            See ya later, <em>Alligator</em>!
+    </div>
+    <div label="Croc">
+        After &apos;while, <em>Crocodile</em>!
+    </div>
+    <div label="Sarcosuchus">
+        Nothing to see here, this tab is <em>extinct</em>!
+        </div>
+    </Tabs>
+</div>
+    );
 }
 
-ReactDOM.render(<div> < HamburgerMenuPage/> < GridExamplesPage/> < ButtonPage/> <Activity/> </div>, document.getElementById('root'));
+class TabContent extends Component {
+    render() {
+        return (
+            <div id="firstTab" className="tabcontent">
+                <h3> Activity </h3>
+                <p>Enter what activity you would like to do from the consumption of food you have consumed.</p>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<div> <HamburgerMenuPage/> <Tabs/> < ButtonPage/> <Activity/> <TabContent/> </div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
