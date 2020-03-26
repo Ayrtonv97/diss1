82862d883fc0a22ac09795946b52cd21d02f81e6
ayrtonvr <ayrtonvanraat@hotmail.co.uk>
2020-03-26 18:02:37 +0000

    Creating new Tab bar and trying to implement dropdown working with tab

---
+++ src/Tab1.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, useTheme, withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import glamorous from "glamorous";
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import styles from "./select.module.css";
+
+function TabPanel(props) {
+    const { children, value, index, } = props;
+
+    return (
+        <Typography>
+            {value === index && <Box p={3}>{children}</Box>}
+        </Typography>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
+
+const StyledTabs = withStyles({
+    indicator: {
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundColor: 'transparent',
+        '& > div': {
+            maxWidth: 120,
+            width: '100%',
+            backgroundColor: '#635ee7',
+        },
+    },
+})(props => <Tabs {...props} TabIndicatorProps={{children: <div/>}}/>);
+
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: 500,
+    },
+    padding: {
+        padding: theme.spacing(3),
+    }
+}));
+
+const ReactTabs = glamorous.div({
+    position: "absolute",
+    right: 400,
+});
+
+const TabsContainer = glamorous.div({
+    position: "fixed",
+    borderBottom: "2px solid #dfdfdf"
+});
+
+export default function FullWidthTabs() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const handleChangeIndex = index => {
+        setValue(index);
+    };
+
+    return (
+        <div className={classes.root}>
+                <StyledTabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    variant="fullWidth"
+                    aria-label="full width tabs example"
+                >
+                    <Tab label="Item One" {...a11yProps(0)} />
+                    <Tab label="Item Two" {...a11yProps(1)} />
+                    <Tab label="Item Three" {...a11yProps(2)} />
+                </StyledTabs>
+            <SwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={value}
+                onChangeIndex={handleChangeIndex}
+            >
+                <TabPanel value={value} index={0} dir={theme.direction}>
+                        <select
+                                style={{padding: 25 + 'px' + 40 + 'px'}}
+                        >
+                            <option value="">Choose Food</option>
+                            <option value="is 500">Apple</option>
+                            <option value="is 1000">ass</option>
+                        </select>
+                </TabPanel>
+                <TabPanel value={value} index={1} dir={theme.direction}>
+                    Item Two
+                </TabPanel>
+                <TabPanel value={value} index={2} dir={theme.direction}>
+                    Item Three
+                </TabPanel>
+            </SwipeableViews>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Navigation from './Navigation/navigateBar';
 import * as serviceWorker from './serviceWorker';
 import ChangeTabs from './changeTab';
+import Tab1 from './Tab1';
 import styles from './select.module.css';
 import './App.css'; //Import here your file styles
 import glamorous from "glamorous";
@@ -12,7 +13,7 @@ import {padding} from "glamor/utils";
 import ActivityForm from "./activityForm";
 
 
-ReactDOM.render(<div> <Navigation/> <ChangeTabs/> </div>, document.getElementById('root'));
+ReactDOM.render(<div> <Navigation/> <Tab1/> </div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
