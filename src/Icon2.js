8d19dae73b76dd08bca2a64c806fc98682225166
AlexMarriott <alex-marriott@live.co.uk>
2020-04-22 17:25:17 +0100

    Tried to refactor all three tabs, need to add submit button

--- src/Icon2.js
+++ src/Icon2.js
@@ -15,32 +15,37 @@ export default function Icon2() {
     const activityEnum = Object.freeze({"Walking": 1, "Running": 2, "Swimming": 3, "Cycling": 4})
     let msg = ""
     let foodChoice  = ""
+    let foodName = ""
     let activity = ""
 
     const foodCaloriesMap = {
-        'Mars Bar': 250,
-        'Apple': 50,
-        'Halo Top': 350
+        'marsbar': 250,
+        'apple': 50,
+        'banana': 20,
+        'crisps':350
     };
 
-    function checkValues(activity, foodChoice) {
+    function checkValues(activity, foodChoice, foodName) {
         if (activity && foodChoice) {
             switch (activity) {
                 //Walking
                 case 1:
-                    msg = "For " + foodChoice  + " of Walking you will burn ..." + walkingCalsPerMinute * foodChoice;
+                    msg = "you'll need to do this exercise for: " +foodChoice / walkingCalsPerMinute + " mins to burn off: " + foodName;
                     break;
                 //Running
                 case 2:
-                    msg = "For " + foodChoice  + " of Running you will burn ..." + runningCalsPerMinute * foodChoice;
+                    msg = "you'll need to do this exercise for: " + foodChoice / runningCalsPerMinute + " mins to burn off: " + foodName;
+
                     break;
                 //Swimming
                 case 3:
-                    msg = "For " + foodChoice  + " of Swimming you will burn ..." + swimmingCalsPerMinute * foodChoice;
+                    msg = "you'll need to do this exercise for: " +foodChoice / swimmingCalsPerMinute + " mins to burn off: " + foodName;
+
                     break;
                 //Cycling
                 case 4:
-                    msg = "For " + foodChoice  + " of Cycling you will burn ..." + cyclingCalsPerMinute * foodChoice;
+                    msg = "you'll need to do this exercise for: " +foodChoice / cyclingCalsPerMinute + " mins to burn off: " + foodName;
+
                     break;
                 default:
                     msg = '';
@@ -51,12 +56,13 @@ export default function Icon2() {
 
     function activityChange(value) {
         activity = value;
-        checkValues(activity, foodChoice)
+        checkValues(activity, foodChoice, foodName)
     }
 
-    function foodChoiceChange(value) {
+    function foodChoiceChange(_foodName, value) {
+        foodName = _foodName
         foodChoice =  value;
-        checkValues(activity, foodChoice)
+        checkValues(activity, foodChoice, foodName)
     }
 
     return (
@@ -68,22 +74,23 @@ export default function Icon2() {
                     onClick={() => activityChange(activityEnum.Cycling)}><MdDirectionsBike/></Button>
             <Button variant="contained" size="medium" color="primary"
                     onClick={() => activityChange(activityEnum.Running)}><MdDirectionsRun/></Button>
-            <Button variant="contained" size="medium" color="primary" onClick={() => activityChange(activityEnum.Swimming)}><FaSwimmer/></Button>
+            <Button variant="contained" size="medium" color="primary"
+                    onClick={() => activityChange(activityEnum.Swimming)}><FaSwimmer/></Button>
             <br/>
             <Button variant="contained" size="medium" color="primary"
-                    onClick={() => foodChoiceChange(15)}>
+                    onClick={() => foodChoiceChange('Mars Bar', foodCaloriesMap.marsbar)}>
                 Mars Bar
             </Button>
             <Button variant="contained" size="medium" color="primary"
-                    onClick={() => foodChoiceChange(30)}>
+                    onClick={() => foodChoiceChange('apple', foodCaloriesMap.apple)}>
                 Apple
             </Button>
             <Button variant="contained" size="medium" color="primary"
-                    onClick={() => foodChoiceChange(45)}>
+                    onClick={() => foodChoiceChange('crips',foodCaloriesMap.crisps)}>
                 Packet of Crisps
             </Button>
             <Button variant="contained" size="medium" color="primary"
-                    onClick={() => foodChoiceChange(60)}>
+                    onClick={() => foodChoiceChange('banana', foodCaloriesMap.banana)}>
                 Banana
             </Button>
             <br/>
--- src/targetedActivity.js
+++ src/targetedActivity.js
@@ -6,18 +6,86 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 class TargetedActivity extends React.Component {
 
-    render() {
+    constructor() {
+        super();
+        this.state = {
+            targeted_cal: '',
+            food: '',
+            foodValue: 0
+        };
+        this.msg = ""
+        this.publish = this.publish.bind(this);
+        this.foodValues =  {"apple": 10, "marsbar": 250, "crisps": 300}
 
+    }
+
+
+    onsubmit = (e) => {
+        this.setState({
+            targeted_cal: e.target.value
+        }
+    )
+            this.publish(this.state.targeted_cal, this.state.foodValue, this.state.food);
+
+
+    }
+    onRadioChange = (e) => {
+        this.setState({
+            food: e.target.value,
+            foodValue: this.foodValues[e.target.value]
+        },
+    );
+
+        this.publish(this.state.targeted_cal, this.foodValues[e.target.value], e.target.value);
+
+
+    }
+
+    publish(targeted_cal, foodValue, food) {
+        this.msg = "You would need to eat: " + targeted_cal / foodValue + " " + food;
+        console.log(targeted_cal, foodValue, food );
+
+    }
+
+    render() {
         return (
-            <form className='targetedActivity' noValidate autoComplete="off">
-                <TextField id="standard-basic" label="Targeted Kcal"/>
+            <form className='targetedActivity' noValidate autoComplete="off" onSubmit={this.mySubmitHandler}>
+                <TextField id="standard-basic"
+                           name="targeted_cal"
+                           label="Targeted Kcal"
+                           placeholder="Enter Kcal here"
+                           value={ this.state.targeted_cal }
+                           onChange={ this.onsubmit }
+                />
+                <input
+                    type='submit'
+                />
+                <br/>
+                {this.msg}
                 <RadioGroup name="gender1">
-                    <FormControlLabel value="Mars Bar" control={<Radio/>} label="Female"/>
-                    <FormControlLabel value="Apple" control={<Radio/>} label="Male"/>
-                    <FormControlLabel value="Packet of crisps" control={<Radio/>} label="Other"/>
+
+                    <FormControlLabel
+                        value="marsbar"
+                        control={<Radio/>}
+                        label="Mars bar"
+                        checked={this.state.food === "marsbar"}
+                        onChange={ this.onRadioChange }
+                    />
+
+                    <FormControlLabel value="apple"
+                                      control={<Radio/>}
+                                      label="Apple"
+                                      checked={this.state.food === "apple"}
+                                      onChange={ this.onRadioChange }
+                    />
+
+                    <FormControlLabel value="crisps"
+                                      control={<Radio/>}
+                                      label="Packet of Crisps"
+                                      checked={this.state.food === "crisps"}
+                                      onChange={ this.onRadioChange }
+                    />
                 </RadioGroup>
             </form>
-        );
-    };
-}
+        )}};
 export default TargetedActivity;
